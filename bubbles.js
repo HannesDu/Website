8a1fcdf493da83f600e88e1d6ab921dc615f6c7b
HannesDu <60402516+HannesDu@users.noreply.github.com>
2020-01-30 19:36:47 -0800

    Update bubbles.js

--- bubbles.js
+++ bubbles.js
@@ -157,7 +157,7 @@ function initEventListeners() {
 function updateCanvasDimensions() {
     canvas.attr({
         height: 500,
-        width: 1000
+        width: 500
     });
     canvasWidth = canvas.width();
     canvasHeight = canvas.height();
