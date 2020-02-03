59b08fe090cd54b0238fa4decf4b233a9a5bfaa4
HannesDu <60402516+HannesDu@users.noreply.github.com>
2020-02-03 09:43:48 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-var red = [0, 100, 63];
+var white = [255, 255, 255];
 // var orange = [40, 100, 60];
 // var green = [75, 100, 40];
 // var blue = [196, 77, 55];
 // var purple = [280, 50, 60];
 
 var myName = "Hannes Du";
-var letterColors = [red];
+var letterColors = [white];
 // , orange, green, blue, purple
 bubbleShape = "circle";
 
